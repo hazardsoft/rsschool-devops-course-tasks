7f5e7e57f560d905ba856a923487fefad5fa7ab6
Henadzi Shutko <hazardsoft@gmail.com>
2024-09-30 00:55:16 +0300

    refactor: apply 'terraform fmt' command

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "rsschool-devops-tfstates"
-    key = "state/terraform.tfstate"
-    region = "eu-central-1"
+    bucket  = "rsschool-devops-tfstates"
+    key     = "state/terraform.tfstate"
+    region  = "eu-central-1"
     encrypt = true
   }
 }
